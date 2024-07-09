e7c43e0171fedc62b3a27a3cff691ddc34419932
Derek Tran <tran.derek.d.@gmail.com>
2024-07-09 14:22:03 -0700

    Add initial development dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import reactConfig from "eslint-plugin-react/configs/recommended.js";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["frontend/**/*.{js,mjs,cjs,jsx}"],
+    reactConfig,
+    languageOptions: { 
+      parserOptions: { 
+        ecmaFeatures: { 
+          jsx: true 
+        } 
+      },
+      globals: globals.browser
+    } 
+  },
+  {
+    files: ["backend/**/*.js"], 
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.node 
+    }
+  },
+  pluginJs.configs.recommended,
+  prettierConfig,
+];

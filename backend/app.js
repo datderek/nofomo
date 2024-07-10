7bdca1cda8cf47211975a403b1bfae804ce5d5f9
Derek Tran <tran.derek.d.@gmail.com>
2024-07-09 18:27:22 -0700

    Create database connection

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
-const express = require('express')
+const express = require('express');
+const postRouter = require('./routes/posts');
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
-// Log request path and method
+// Middleware - Log request path and method
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use('/api/posts', postRouter);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`)
-})
+  console.log(`Listening on port ${port}!`);
+});
---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+// Create a connection pool with default settings
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = connection;

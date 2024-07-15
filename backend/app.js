6703dba6afd48433974d0a2da5adbec652ab84af
Derek Tran <tran.derek.d.@gmail.com>
2024-07-15 11:12:59 -0700

    Add CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./routes/posts');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Middleware - Log request path and method
+// --- Middleware --- //
+// Allow CORS
+app.use(cors({ origin: `${process.env.CORS_ORIGIN}` }));
+
+// Log request path and method
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
+// Parse request body to JSON
 app.use(express.json());
 
+// --- Routes --- //
 app.use('/api/posts', postRouter);
 
 app.listen(port, () => {

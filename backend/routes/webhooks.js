55d81109d36a8b7d4ba0302e24c4e182f09bb19a
Derek Tran <tran.derek.d.@gmail.com>
2024-07-25 12:36:29 -0700

    Sync database with user creation

--- backend/routes/webhooks.js
+++ backend/routes/webhooks.js
@@ -1,12 +1,14 @@
-const { Webhook } = require('svix');
 const express = require('express');
+const db = require('../config/database');
+const { Webhook } = require('svix');
 
 const router = express.Router();
 
 router.use(express.raw());
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
   if (!WEBHOOK_SECRET) {
     throw new Error('You need a WEBHOOK_SECRET in your .env');
   }
@@ -22,41 +24,63 @@ router.post('/', async (req, res) => {
 
   // If there are no Svix headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400,
-    });
+    throw new Error('Error occured -- no svix headers');
   }
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt;
+  let event;
 
   // Attempt to verify the incoming webhook
   // If successful, the payload will be available from 'evt'
   // If the verification fails, error out and  return error code
   try {
-    evt = wh.verify(payload, {
+    event = wh.verify(payload, {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
       'svix-signature': svix_signature,
     });
   } catch (err) {
-    console.log('Error verifying webhook:', err.message);
-    return res.status(400).json({
-      success: false,
-      message: err.message,
-    });
+    next(err);
   }
 
-  const { id } = evt.data;
-  const eventType = evt.type;
-  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
-  console.log('Webhook body:', evt.data);
+  switch (event.type) {
+    case 'user.created': {
+      const {
+        id: clerkId,
+        username,
+        email_addresses: emailAddresses,
+        first_name: firstName,
+        last_name: lastName,
+      } = event.data;
+      const { email_address: email } = emailAddresses[0];
+
+      try {
+        const sql =
+          'INSERT INTO `users` (`clerk_id`, `username`, `email`, `first_name`, `last_name`) VALUES (?, ?, ?, ?, ?)';
+        await db.execute(sql, [clerkId, username, email, firstName, lastName]);
+      } catch (err) {
+        next(err);
+      }
+      break;
+    }
+    default: {
+      throw new Error('No handler defined for event of type: ' + event.type);
+    }
+  }
 
   return res.status(200).json({
-    success: true,
+    status: 'success',
     message: 'Webhook received',
+  });
+});
+
+router.use((err, req, res, next) => {
+  console.log(err.message);
+  return res.status(400).send({
+    status: 'error',
+    messsage: err.message,
   });
 });
 

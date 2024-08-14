5342f190190cc1b2dd9687ebdb3c2b44d7634562
Derek Tran <tran.derek.d.@gmail.com>
2024-08-14 16:22:27 -0700

    Remove bio from banner

--- frontend/src/components/ProfileBanner/ProfileBanner.jsx
+++ frontend/src/components/ProfileBanner/ProfileBanner.jsx
@@ -2,7 +2,7 @@ import BannerButton from './BannerButton';
 
 export default function ProfileBanner({ user, belongsToCurrUser }) {
   return (
-    <div className="w-full px-4 py-2 grid grid-cols-3 grid-rows-[repeat(3,min-content)_1fr] border border-black">
+    <div className="w-full px-4 py-2 grid grid-cols-3 grid-rows-[1fr_min-content_1fr]">
       <div className="col-span-1 row-span-full mx-auto">
         <img
           className="h-[150px] aspect-square rounded-full bg-gray-300"
@@ -10,10 +10,10 @@ export default function ProfileBanner({ user, belongsToCurrUser }) {
           alt={`Profile picture of ${user.username}`}
         />
       </div>
-      <div className="col-start-3 col-end-4 row-start-1 row-end-3 flex justify-center items-center">
+      <div className="col-start-3 col-end-4 row-start-1 row-end-3 self-center">
         <BannerButton belongsToCurrUser={belongsToCurrUser} />
       </div>
-      <div className="col-start-2 col-end-3 row-start-1 row-end-1 text-2xl leading-tight ">
+      <div className="col-start-2 col-end-3 row-start-1 row-end-1 self-end text-2xl leading-tight">
         {user.firstName} {user.lastName}
       </div>
       <div className="text-lg leading-tight mb-2">@{user.username}</div>
@@ -21,11 +21,6 @@ export default function ProfileBanner({ user, belongsToCurrUser }) {
         <div>XX posts</div>
         <div>XX followers</div>
         <div>XX following</div>
-      </div>
-      <div className="col-start-2 col-end-4 text-sm">
-        This would be where my bio is. Note that bio should not exceed probably
-        around two hundred characters. This example has a length of about 150
-        characters.
       </div>
     </div>
   );

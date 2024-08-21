f7596b7435c1761d1986e144a1d77885f2dffe29
Derek Tran <tran.derek.d.@gmail.com>
2024-08-21 15:14:46 -0700

    Dashboard queries for following posts

--- frontend/src/components/NavigationPanel/NavigationPanel.jsx
+++ frontend/src/components/NavigationPanel/NavigationPanel.jsx
@@ -1,19 +1,21 @@
 export default function NavigationPanel() {
   return (
-    <div className="flex flex-col gap-y-4 items-center w-full">
-      <div className="h-[150px] w-[150px] rounded-full bg-gray-300"></div>
-      <div>
-        <div className="text-center text-2xl font-medium">Derek Tran</div>
-        <div className="text-center">@datderek</div>
-      </div>
-      <div className="w-[150px] px-4 py-2 rounded-xl text-left bg-gray-800 text-white font-medium text-xl">
-        Feed
-      </div>
-      <div className="w-[150px] px-4 py-2 rounded-xl text-left font-medium text-xl">
-        Explore
-      </div>
-      <div className="w-[150px] px-4 py-2 rounded-xl text-left font-medium text-xl">
-        Calendar
+    <div className="sticky top-navbar h-min p-2">
+      <div className="flex flex-col gap-y-4 items-center w-full">
+        <div className="h-[150px] w-[150px] rounded-full bg-gray-300"></div>
+        <div>
+          <div className="text-center text-2xl font-medium">Derek Tran</div>
+          <div className="text-center">@datderek</div>
+        </div>
+        <div className="w-[150px] px-4 py-2 rounded-xl text-left bg-gray-800 text-white font-medium text-xl">
+          Feed
+        </div>
+        <div className="w-[150px] px-4 py-2 rounded-xl text-left font-medium text-xl">
+          Explore
+        </div>
+        <div className="w-[150px] px-4 py-2 rounded-xl text-left font-medium text-xl">
+          Calendar
+        </div>
       </div>
     </div>
   );
--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,17 +1,16 @@
 import { useOutletContext } from 'react-router-dom';
 import NavigationPanel from '../components/NavigationPanel/NavigationPanel';
+import useFollowingPosts from '../hooks/useFollowingPosts';
 
 export default function DashboardPage() {
-  const [user] = useOutletContext();
-  console.log(user);
-
+  const [currUser, getToken] = useOutletContext();
+  const { posts, hasMorePosts, loadMorePosts } = useFollowingPosts(getToken);
+  console.log(currUser, posts, hasMorePosts, loadMorePosts);
   return (
     <>
       <div className="w-screen min-h-screen flex justify-center">
         <div className="max-w-[1200px] h-[1000px] w-full grid grid-cols-[250px_1fr_250px] gap-x-6">
-          <div className="sticky top-navbar h-min p-2">
-            <NavigationPanel />
-          </div>
+          <NavigationPanel />
           <div className="flex-grow w-full h-full bg-red-200"></div>
           <div className="sticky top-navbar flex-none h-[400px] bg-green-200"></div>
         </div>

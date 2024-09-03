56f6678ff222607adc1826688db217ae8fe8b887
Derek Tran <tran.derek.d.@gmail.com>
2024-09-03 12:33:13 -0700

    Navigation panel displays current user information

--- frontend/src/components/NavigationPanel/NavigationPanel.jsx
+++ frontend/src/components/NavigationPanel/NavigationPanel.jsx
@@ -1,13 +1,18 @@
+import { useOutletContext } from 'react-router-dom';
 import ProfileIcon from '../shared/ProfileIcon';
 
 export default function NavigationPanel() {
+  const [currUser] = useOutletContext();
+
   return (
     <div className="sticky top-navbar h-min p-2">
       <div className="flex flex-col gap-y-4 items-center w-full">
         <ProfileIcon width={'h-36'} />
         <div>
-          <div className="text-center text-2xl font-medium">Derek Tran</div>
-          <div className="text-center">@datderek</div>
+          <div className="text-center text-2xl font-medium">
+            {currUser.fullName}
+          </div>
+          <div className="text-center">@{currUser.username}</div>
         </div>
         <div className="w-[150px] px-4 py-2 rounded-xl text-left bg-gray-800 text-white font-medium text-xl">
           Feed

aea405a8f124f14dde4c0dd79ac8970678e03102
Derek Tran <tran.derek.d.@gmail.com>
2024-09-03 12:22:44 -0700

    Add default size for ProfileIcon

--- frontend/src/components/shared/ProfileIcon.jsx
+++ frontend/src/components/shared/ProfileIcon.jsx
@@ -1,7 +1,7 @@
 import { useOutletContext } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-export default function ProfileIcon({ size }) {
+export default function ProfileIcon({ size = 'h-36' }) {
   const [currUser] = useOutletContext();
 
   return (
